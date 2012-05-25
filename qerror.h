8969bcd523c36d8d94535bd5ddd924f6fe60ea61
Amos Kong <akong@redhat.com>
2012-05-25 09:44:48 +0800

    qerror: add QERR_OVERFLOW
    Signed-off-by: Amos Kong <akong@redhat.com>

--- qerror.h
+++ qerror.h
@@ -151,6 +151,9 @@ QError *qobject_to_qerror(const QObject *obj);
 #define QERR_INVALID_PARAMETER_VALUE \
     "{ 'class': 'InvalidParameterValue', 'data': { 'name': %s, 'expected': %s } }"
 
+#define QERR_OVERFLOW \
+    "{ 'class': 'Overflow', 'data': {} }"
+
 #define QERR_INVALID_PASSWORD \
     "{ 'class': 'InvalidPassword', 'data': {} }"
 
